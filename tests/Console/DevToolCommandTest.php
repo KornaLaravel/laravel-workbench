5c14acc16c4c26830ab72f6b702dfdaa8995396c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-10 06:53:40 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Console/DevToolCommandTest.php
+++ tests/Console/DevToolCommandTest.php
@@ -2,8 +2,8 @@
 
 namespace Orchestra\Workbench\Tests\Console;
 
-use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 
 class DevToolCommandTest extends CommandTestCase
 {
